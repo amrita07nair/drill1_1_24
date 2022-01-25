391870ae1831ed13edbcf384685d2157bddb2f63
amrita07nair <amrita07nair@gmail.com>
2022-01-24 23:44:32 -0500

    q3 done

--- drill1.py
+++ drill1.py
@@ -31,5 +31,8 @@ print(arr2D)
 """
 I was feeling a bit confused about the terms. I was in class but needed a more basic and easy way to understand the 2 methods.
 This article helped me understand the question and concepts: https://towardsdatascience.com/stochastic-gradient-descent-clearly-explained-53d239905d31
-To answer the question, we save time by using randomness to find the minimum. In the standard method, a random point is selected at first and the step size is founs. Then 
+To answer the question, we save time by using randomness to find the minimum. In the standard method, a random point is selected at first and the step size is found.
+Then the new parameters are computed and the process repeats to find the minimum. This requires lots of repeat computations. It is also ideal to have a small step size because if the step is too big, 
+you risk stepping over the minimun. With the Stochastic method, there is not steps perso, but rather, at every itteration a new point is selected. This prevents the risk of stepping over the min and having to compute parameters and steps every iteration. 
+The article talks about another methos that combines the speed of the Stochastic method and the precision of the standard method where random points are selected and go though a bit of the standard gradient method. 
 """
